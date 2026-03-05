40217c54d149803ca5b70acd8f4aad9a5785c093
IronFeniks <iron.shizo@gmail.com>
2026-03-06 00:23:53 +0400

    config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+# Токен бота (получите у @BotFather если еще нет)
+TOKEN = "8781867816:AAHTUiftksAirLCllqFoALgeCUAJ0nNEkP0"
+
+# ID группы и темы (ваши данные)
+GROUP_ID = -1003300908374
+TOPIC_ID = 3830
+
+# Ссылка на Яндекс Таблицу в формате CSV
+# ЗАМЕНИТЕ НА СВОЮ ССЫЛКУ ПОЗЖЕ
+YANDEX_TABLE_URL = "https://docs.yandex.ru/i/oX1mZgw-1NdaLg?format=csv"
+
+# Настройки кэширования
+CACHE_TTL = 300  # 5 минут
